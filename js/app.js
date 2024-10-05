eb8bea3ed7c23e5db70bb78a7763a73bdc253662
web-mahadihasan <mehedihasanmilu7@gmail.com>
2024-10-05 23:41:25 +0600

    I starting this project and complete navbar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+// Hamburger button functionality
+document.getElementById("hamburger-btn").addEventListener("click", () => {
+  const btn = document.getElementById("hamburger-menu");
+  btn.classList.toggle("hidden")
+  console.log(btn);
+});
