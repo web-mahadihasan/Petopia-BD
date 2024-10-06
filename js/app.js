c941c3b9d96a1c181a2a306caf6ebba6a7971d2f
web-mahadihasan <mehedihasanmilu7@gmail.com>
2024-10-06 15:33:03 +0600

    I haved added like button functionality

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ document.getElementById("hamburger-btn").addEventListener("click", () => {
 
 // gets all elements 
 const petCardsContainer = document.getElementById("pet-cards-container");
+const petsNotFoundContainer = document.getElementById("pets-not-found");
+
+
 
 // Load category btn 
 const loadCategory = async () => {
@@ -25,13 +28,26 @@ const loadPetsCard = async () => {
   displayPetsData(allPets);
 }
 
+let lastCategoryBtnClick = null;
 // Category btn functionality & show category pets
 const categoriesPetsDisplay = async (category) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`);
   const data = await res.json();
   const categoryData = data.data;
   displayPetsData(categoryData);
+
+  //category btn active color add & remove
+  const allCategoryBtn = document.getElementsByClassName("category-btn");
+  for(const btn of allCategoryBtn){
+    btn.classList.add("border-gray-200", "rounded-xl");
+    btn.classList.remove("rounded-full", "border-primary-color/50", "bg-primary-color/10");
+  }
+  
+  const activeStyles = document.getElementById(`${category}`);
+    activeStyles.classList.remove("border-gray-200", "rounded-xl");
+    activeStyles.classList.add("rounded-full", "border-primary-color/50", "bg-primary-color/10");
 }
+
 
 // Display category btn
 const displayCategoriesBtn = (categories) => {
@@ -40,7 +56,7 @@ const displayCategoriesBtn = (categories) => {
   categories.forEach(btnData => {
     const div = document.createElement("div");
     div.innerHTML = `
-      <button onclick="categoriesPetsDisplay('${btnData.category}')" class="flex items-center gap-1 py-3 px-16 border border-gray-200 rounded-xl justify-center ">
+      <button id="${btnData.category}" onclick="categoriesPetsDisplay('${btnData.category}')" class="category-btn flex items-center gap-1 py-3 px-16 border border-gray-200 rounded-xl justify-center ">
         <span><img src="${btnData.category_icon}" alt="" class="w-12"></span>
         <span class="font-bold text-text-black font-inter text-2xl">${btnData.category}</span>
       </button>
@@ -54,46 +70,62 @@ const displayCategoriesBtn = (categories) => {
 const displayPetsData = (allPets) => {
   petCardsContainer.innerHTML = "";
   
+  if(allPets.length === 0){
+    petsNotFoundContainer.classList.remove("hidden");
+  }else{
+    petsNotFoundContainer.classList.add("hidden");
+  }
+
   sortedData(allPets);
   
   allPets.forEach(petsData => {
-    const { breed, image, pet_name, date_of_birth, gender, price } = petsData;
+    const {petId, breed, image, pet_name, date_of_birth, gender, price } = petsData;
     const div = document.createElement("div");
     div.innerHTML = `
       <div class=" bg-base-100 shadow border border-gray-100 p-4 gap-4 rounded-xl">
         <figure>
           <img src="${image}" alt="Shoes"
-          class="rounded-xl h-[200px] w-full" />
+          class="rounded-xl h-[200px] w-full object-cover" />
         </figure>
         <div class="py-4 space-y-1">
-          <h2 class="font-inter font-bold text-xl text-text-black">${pet_name || "Not found"}</h2>
+          <h2 class="font-inter font-bold text-xl text-text-black">${
+            pet_name || "Not found"
+          }</h2>
 
           <div class="text-secondary-color font-lato">
             <span><i class="ri-function-add-line text-xl font-medium align-middle"></i></span>
-            <span class="ml-1">Breed: <span>${breed || "Not found"}</span></span>
+            <span class="ml-1">Breed: <span>${
+              breed || "Not found"
+            }</span></span>
           </div>
           <div class="text-secondary-color font-lato">
             <span><i class="ri-calendar-schedule-line text-xl font-medium align-middle"></i></span>
-            <span class="ml-1">Birth: <span>${date_of_birth || "Not found"}</span></span>
+            <span class="ml-1">Birth: <span>${
+              date_of_birth || "Not found"
+            }</span></span>
           </div>
           <div class="text-secondary-color font-lato">
             <span><i class="ri-share-line text-xl font-medium align-middle"></i></span>
-            <span class="ml-1">Gender: <span>${gender || "Not found"}</span></span>
+            <span class="ml-1">Gender: <span>${
+              gender || "Not found"
+            }</span></span>
           </div>
           <div class="pb-1 text-secondary-color font-lato">
             <span><i class="ri-money-dollar-circle-line text-xl font-medium align-middle"></i></span>
-            <span class="ml-1">Price : <span>Price : ${price || "Not available"}</span></span>
+            <span class="ml-1">Price : <span>Price : ${
+              price || "Not available"
+            }</span></span>
           </div>
 
           <div class="border-t border-gray-200"></div>
 
           <div class="pt-3 flex justify-between items-center">
-            <button class="flex flex-col py-1 px-4 rounded-md border border-primary-color/20 font-lato text-xl text-gray-500">
-              <span><i class="ri-thumb-up-line"></i></span>
-              <span class="hidden"><i class="ri-thumb-up-fill"></i></span>
+            <button onclick="likedPets(${petId})" class="flex flex-col py-1 px-4 rounded-md border border-primary-color/20 font-lato text-xl text-gray-500">
+              <span id="normal-${petId}"><i class="ri-thumb-up-line"></i></span>
+              <span id="liked-${petId}" class="hidden text-primary-color"><i class="ri-thumb-up-fill"></i></span>
             </button>
             <button class="py-1 px-4 rounded-md border border-primary-color/20 font-semibold font-lato text-lg text-primary-color">Adopt</button>
-            <button class="py-1 px-4 rounded-md border border-primary-color/20 font-semibold font-lato text-lg text-primary-color">Details</button>
+            <button onclick="showDetails(${petId})" class="py-1 px-4 rounded-md border border-primary-color/20 font-semibold font-lato text-lg text-primary-color">Details</button>
           </div>
         </div>
       </div>
@@ -104,7 +136,7 @@ const displayPetsData = (allPets) => {
 }
 
 
-
+// Data sorted by price 
 const sortedData = (sortData) => {
   const sortBtn = document.getElementById("sort-btn");
   sortBtn.addEventListener("click", () => {
@@ -124,6 +156,29 @@ const sortedData = (sortData) => {
   });
 };
 
+// Like button functionality
+const likedPets = async (id) => {
+  const likeUnClick = document.getElementById(`normal-${id}`);
+  likeUnClick.classList.add("hidden");
+  const likeClick = document.getElementById(`liked-${id}`);
+  likeClick.classList.remove("hidden")
+
+  const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+  const data = await res.json();
+  const petDataByID = data.petData;
+  likePetsImageDisplay(petDataByID);
+}
+
+const likePetsImageDisplay = (petDataByID) => {
+  const likedPetImagesContainer =document.getElementById("liked-pet-image-container");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div>
+      <img src="${petDataByID.image}" alt="" class="h-[120px] w-full rounded-lg object-cover">
+    </div>
+  `;
+  likedPetImagesContainer.append(div);
+};
 
 
 loadCategory();
